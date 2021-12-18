fcfa98b3d687a8d633bd932341174fecf8b187a2
Jean-Christian Denis <contact@jcdenis.fr>
2021-12-18 22:39:06 +0100

    start moveing installation files to namespace

--- src/Install/Prepend.php
+++ src/Install/Prepend.php
@@ -24,43 +24,75 @@ class Prepend extends BasePrepend
     {
         /* No configuration ? start installalation process */
         if (!is_file(DOTCLEAR_CONFIG_PATH)) {
-            $this->wizard();
+            new Wizard($this);
         } else {
             parent::__construct();
-            $this->install();
+            new Install($this);
         }
-echo 'install : inc/admin/install/xxx.php : structure only ';
+exit('install : inc/admin/install/xxx.php : structure only');
     }
 
-    protected function install()
+    public static function systemCheck($con, &$err)
     {
-        $can_install = true;
-        $err         = '';
+        $err = [];
 
-        # Loading locales for detected language
-        $dlang = Http::getAcceptLanguage();
-        if ($dlang != 'en') {
-            L10n::init($dlang);
-            L10n::set(static::root(DOTCLEAR_L10N_DIR, $dlang, 'date'));
-            L10n::set(static::root(DOTCLEAR_L10N_DIR, $dlang, 'main'));
-            L10n::set(static::root(DOTCLEAR_L10N_DIR, $dlang, 'plugins'));
+        if (version_compare(phpversion(), '7.4', '<')) {
+            $err[] = sprintf(__('PHP version is %s (7.4 or earlier needed).'), phpversion());
         }
 
-        if (!defined('DOTCLEAR_MASTER_KEY') || DOTCLEAR_MASTER_KEY == '') {
-            $can_install = false;
-            $err         = '<p>' . __('Please set a master key (DOTCLEAR_MASTER_KEY) in configuration file.') . '</p>';
+        if (!function_exists('mb_detect_encoding')) {
+            $err[] = __('Multibyte string module (mbstring) is not available.');
         }
-echo 'install: index.php : structure only ';
-    }
 
-    protected function wizard()
-    {
-        # Loading locales for detected language
-        $dlang = Http::getAcceptLanguage();
-        if ($dlang != 'en') {
-            L10n::init($dlang);
-            L10n::set(static::root(DOTCLEAR_L10N_DIR, $dlang, 'main'));
+        if (!function_exists('iconv')) {
+            $err[] = __('Iconv module is not available.');
         }
-echo 'install: wizard.php : structure only ';
+
+        if (!function_exists('ob_start')) {
+            $err[] = __('Output control functions are not available.');
+        }
+
+        if (!function_exists('simplexml_load_string')) {
+            $err[] = __('SimpleXML module is not available.');
+        }
+
+        if (!function_exists('dom_import_simplexml')) {
+            $err[] = __('DOM XML module is not available.');
+        }
+
+        $pcre_str = base64_decode('w6nDqMOgw6o=');
+        if (!@preg_match('/' . $pcre_str . '/u', $pcre_str)) {
+            $err[] = __('PCRE engine does not support UTF-8 strings.');
+        }
+
+        if (!function_exists('spl_classes')) {
+            $err[] = __('SPL module is not available.');
+        }
+
+        if ($con->syntax() == 'mysql') {
+            if (version_compare($con->version(), '4.1', '<')) {
+                $err[] = sprintf(__('MySQL version is %s (4.1 or earlier needed).'), $con->version());
+            } else {
+                $rs     = $con->select('SHOW ENGINES');
+                $innodb = false;
+                while ($rs->fetch()) {
+                    if (strtolower($rs->f(0)) == 'innodb' && strtolower($rs->f(1)) != 'disabled' && strtolower($rs->f(1)) != 'no') {
+                        $innodb = true;
+
+                        break;
+                    }
+                }
+
+                if (!$innodb) {
+                    $err[] = __('MySQL InnoDB engine is not available.');
+                }
+            }
+        } elseif ($con->driver() == 'pgsql') {
+            if (version_compare($con->version(), '8.0', '<')) {
+                $err[] = sprintf(__('PostgreSQL version is %s (8.0 or earlier needed).'), $con->version());
+            }
+        }
+
+        return count($err) == 0;
     }
 }
