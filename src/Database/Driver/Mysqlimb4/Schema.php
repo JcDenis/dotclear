f6435099a96788272e13aca1156f7140978ab986
Jean-Christian Denis <contact@jcdenis.fr>
2021-12-18 20:39:08 +0100

    import class from clearbricks/database and reorganize drivers

---
+++ src/Database/Driver/Mysqlimb4/Schema.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @class Schema with mysqlimb4 driver
+ *
+ * @package Dotclear
+ * @subpackage Database
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Database\Driver\Mysqlimb4;
+
+use Dotclear\Database\Driver\Mysqli\Schema as Mysqli;
+
+class Schema extends Mysqli
+{
+    public function db_create_table(string $name, array $fields): void
+    {
+        $a = [];
+
+        foreach ($fields as $n => $f) {
+            $type    = $f['type'];
+            $len     = (integer) $f['len'];
+            $default = $f['default'];
+            $null    = $f['null'];
+
+            $type = $this->udt2dbt($type, $len, $default);
+            $len  = $len > 0 ? '(' . $len . ')' : '';
+            $null = $null ? 'NULL' : 'NOT NULL';
+
+            if ($default === null) {
+                $default = 'DEFAULT NULL';
+            } elseif ($default !== false) {
+                $default = 'DEFAULT ' . $default . ' ';
+            } else {
+                $default = '';
+            }
+
+            $a[] = $this->con->escapeSystem($n) . ' ' .
+                $type . $len . ' ' . $null . ' ' . $default;
+        }
+
+        $sql = 'CREATE TABLE ' . $this->con->escapeSystem($name) . " (\n" .
+        implode(",\n", $a) .
+            "\n) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
+
+        $this->con->execute($sql);
+    }
+}
---
+++ src/Database/Exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @class Database Exception
+ *
+ * @package Dotclear
+ * @subpackage Database
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Database;
+
+use Dotclear\Exception as BaseException;
+
+class Exception extends BaseException { }
