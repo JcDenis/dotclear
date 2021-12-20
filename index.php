03d65e19df6977c4b137f6d025ce6207e92a48b1
Jean-Christian Denis <contact@jcdenis.fr>
2021-12-20 09:38:04 +0100

    move example files for an easier first installation

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 /**
+ * @brief Dotclear process example
+ *
+ * This file is the public (frontend) acces point
+ * of the default blog.
+ *
  * @package Dotclear
+ * @subpackage Process
  *
  * @copyright Olivier Meunier & Association Dotclear
  * @copyright GPL-2.0-only
@@ -14,4 +20,6 @@ if (isset($_SERVER['DC_BLOG_ID'])) {
     define('DC_BLOG_ID', 'default');
 }
 
-require dirname(__FILE__) . '/inc/public/prepend.php';
+require implode(DIRECTORY_SEPARATOR, [dirname(__FILE__), 'src', 'Process.php']);
+
+new Dotclear\Process('public');
