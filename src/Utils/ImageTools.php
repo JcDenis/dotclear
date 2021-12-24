2d5ed6f0bcf287ca206c0e847b582f6c9376d196
Jean-Christian Denis <contact@jcdenis.fr>
2021-12-24 21:06:26 +0100

    Fix type hint on resize method

--- src/Utils/ImageTools.php
+++ src/Utils/ImageTools.php
@@ -229,11 +229,11 @@ class ImageTools
         $imgWidth  = $this->getW();
         $imgHeight = $this->getH();
 
-        if (strpos($WIDTH, '%', 0)) {
+        if (strpos((string) $WIDTH, '%', 0)) {
             $WIDTH = $imgWidth * $WIDTH / 100;
         }
 
-        if (strpos($HEIGHT, '%', 0)) {
+        if (strpos((string) $HEIGHT, '%', 0)) {
             $HEIGHT = $imgHeight * $HEIGHT / 100;
         }
 
@@ -305,6 +305,14 @@ class ImageTools
         if ($_h < 1) {
             $_h = 1;
         }
+
+        // convert float to int
+        settype($decalW, 'int');
+        settype($decalH, 'int');
+        settype($_w, 'int');
+        settype($_h, 'int');
+        settype($cropW, 'int');
+        settype($cropH, 'int');
 
         # truecolor is 24 bit RGB, ie. 3 bytes per pixel.
         $this->memoryAllocate($_w, $_h, 3);
