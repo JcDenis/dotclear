ecf2e289090628bcac77410aea6b84aecd097f6c
Jean-Christian Denis <contact@jcdenis.fr>
2021-12-27 20:11:10 +0100

    review class

--- src/Core/Behaviors.php
+++ src/Core/Behaviors.php
@@ -31,25 +31,26 @@ class Behaviors
     }
 
     /**
-     * Adds a new behavior to behaviors stack. <var>$func</var> must be a valid
+     * Adds a new behavior to behaviors stack. <var>$callback</var> must be a valid
      * and callable callback.
      *
-     * @param      string    $behavior  The behavior
-     * @param      callable  $func      The function
+     * @param   string          $behavior   The behavior
+     * @param   string|array    $callback   The function
      */
-    public function add(string $behavior, $func): void
+    public function add(string $behavior, string|array $callback): void
     {
-        if (is_callable($func)) {
-            $this->behaviors[$behavior][] = $func;
+        # Silently failed non callable function
+        if (is_callable($callback)) {
+            $this->behaviors[$behavior][] = $callback;
         }
     }
 
     /**
      * Determines if behavior exists in behaviors stack.
      *
-     * @param      string  $behavior  The behavior
+     * @param   string  $behavior   The behavior
      *
-     * @return     bool    True if behavior exists, False otherwise.
+     * @return  bool    True if behavior exists, False otherwise.
      */
     public function has(string $behavior): bool
     {
@@ -59,9 +60,9 @@ class Behaviors
     /**
      * Gets the behaviors stack (or part of).
      *
-     * @param      string  $behavior  The behavior
+     * @param   string  $behavior   The behavior
      *
-     * @return     array   The behaviors.
+     * @return  array   The behaviors.
      */
     public function get(string $behavior = ''): array
     {
@@ -85,12 +86,12 @@ class Behaviors
      * Every parameters added after <var>$behavior</var> will be pass to
      * behavior calls. Core will be injected as first argument.
      *
-     * @param      string  $behavior  The behavior
-     * @param      mixed   ...$args   The arguments
+     * @param   string  $behavior   The behavior
+     * @param   mixed   ...$args    The arguments
      *
-     * @return     mixed   Behavior concatened result
+     * @return  mixed   Behavior concatened result
      */
-    public function call(string $behavior, ...$args)
+    public function call(string $behavior, mixed ...$args): mixed
     {
         return $this->callArray($behavior, $args);
     }
@@ -99,12 +100,12 @@ class Behaviors
     {
         if (isset($this->behaviors[$behavior])) {
             $res = '';
-            /* add core instance to every call */
+            # add core instance to every call
             array_unshift($args, $this->core);
 
-            foreach ($this->behaviors[$behavior] as $f) {
-                $this->trace($f, $args);
-                $res .= call_user_func_array($f, $args);
+            foreach ($this->behaviors[$behavior] as $callback) {
+                $this->trace($callback, $args);
+                $res .= call_user_func_array($callback, $args);
             }
 
             return $res;
@@ -114,16 +115,16 @@ class Behaviors
     /**
      * Trace behaviors call
      *
-     *  For debug purpose, you can define a callable function.
+     * For debug purpose, you can define a callable function.
      *
-     * @param  string $f Called function
-     * @param  array  $a Called function arguments
+     * @param  string   $callback   Called function
+     * @param  array    $args       Called function arguments
      */
-    private function trace($f, $a): void
+    private function trace($callback, $args): void
     {
         if (defined('DOTCLEAR_BEHAVIOR_TRACE') && is_callable(DOTCLEAR_BEHAVIOR_TRACE)) {
             try {
-                call_user_func(DOTCLEAR_BEHAVIOR_TRACE, $f, $a);
+                call_user_func(DOTCLEAR_BEHAVIOR_TRACE, $callback, $args);
             } catch (Exception $e) {
             }
         }
