622c2dd41d42bd74392ac664d96143770e912707
Jean-Christian Denis <contact@jcdenis.fr>
2021-12-30 18:56:28 +0100

    fix new Admin Page process for Blog and BlogPref Page

--- src/Admin/Page/Blog.php
+++ src/Admin/Page/Blog.php
@@ -31,28 +31,50 @@ if (!defined('DOTCLEAR_PROCESS') || DOTCLEAR_PROCESS != 'Admin') {
 
 class Blog extends Page
 {
-    public function __construct(Core $core)
+    private $blog_id   = '';
+    private $blog_name = '';
+    private $blog_desc = '';
+    private $blog_url  = '';
+
+    protected function getPermissions(): string
     {
-        parent::__construct($core);
+        return 'usage,contentadmin';
+    }
 
-        $this->checkSuper();
+    protected function getPagePrepend(): ?bool
+    {
+        # If there's a blog id, process blog pref
+        if (!empty($_REQUEST['id'])) {
+            $blog_pref = new BlogPref($this->core, $this->handler, false);
 
-        $blog_id   = '';
-        $blog_url  = '';
-        $blog_name = '';
-        $blog_desc = '';
+            return $blog_pref->pageProcess();
+        }
+
+        # Page setup
+        $this
+            ->setPageHelp('core_blog_new')
+            ->setPageTitle(__('New blog'))
+            ->setPageHead(
+                static::jsConfirmClose('blog-form')
+            )
+            ->setPageBreadcrumb([
+                __('System')   => '',
+                __('Blogs')    => $this->core->adminurl->get('admin.blogs'),
+                __('New blog') => ''
+            ])
+        ;
 
         # Create a blog
         if (!isset($_POST['id']) && (isset($_POST['create']))) {
             $cur       = $this->core->con->openCursor($this->core->prefix . 'blog');
-            $blog_id   = $cur->blog_id   = $_POST['blog_id'];
-            $blog_url  = $cur->blog_url  = $_POST['blog_url'];
-            $blog_name = $cur->blog_name = $_POST['blog_name'];
-            $blog_desc = $cur->blog_desc = $_POST['blog_desc'];
+            $this->blog_id   = $cur->blog_id   = $_POST['blog_id'];
+            $this->blog_url  = $cur->blog_url  = $_POST['blog_url'];
+            $this->blog_name = $cur->blog_name = $_POST['blog_name'];
+            $this->blog_desc = $cur->blog_desc = $_POST['blog_desc'];
 
             try {
                 # --BEHAVIOR-- adminBeforeBlogCreate
-                $this->core->behaviors->call('adminBeforeBlogCreate', $cur, $blog_id);
+                $this->core->behaviors->call('adminBeforeBlogCreate', $cur, $this->blog_id);
 
                 $this->core->addBlog($cur);
 
@@ -62,14 +84,14 @@ class Blog extends Page
                 $blog_settings->system->put('lang', $this->core->auth->getInfo('user_lang'));
                 $blog_settings->system->put('blog_timezone', $this->core->auth->getInfo('user_tz'));
 
-                if (substr($blog_url, -1) == '?') {
+                if (substr($this->blog_url, -1) == '?') {
                     $blog_settings->system->put('url_scan', 'query_string');
                 } else {
                     $blog_settings->system->put('url_scan', 'path_info');
                 }
 
                 # --BEHAVIOR-- adminAfterBlogCreate
-                $core->behaviors->call('adminAfterBlogCreate', $cur, $blog_id, $blog_settings);
+                $this->core->behaviors->call('adminAfterBlogCreate', $cur, $this->blog_id, $blog_settings);
 
                 static::addSuccessNotice(sprintf(__('Blog "%s" successfully created'), html::escapeHTML($cur->blog_name)));
                 $this->core->adminurl->redirect('admin.blog', ['id' => $cur->blog_id, 'edit_blog_mode' => 1]);
@@ -78,66 +100,53 @@ class Blog extends Page
             }
         }
 
-        if (!empty($_REQUEST['id'])) {
-            define('EDIT_BLOG_MODE', true);
-            new BlogPref($core);
-            return;
-        } else {
-            $this->open(__('New blog'), static::jsConfirmClose('blog-form'),
-                $this->breadcrumb(
-                    [
-                        __('System')   => '',
-                        __('Blogs')    => $this->core->adminurl->get('admin.blogs'),
-                        __('New blog') => ''
-                    ])
-            );
+        return true;
+    }
 
-            echo
-            '<form action="' . $this->core->adminurl->get('admin.blog') . '" method="post" id="blog-form">' .
+    protected function getPageContent(): void
+    {
+        echo
+        '<form action="' . $this->core->adminurl->get('admin.blog') . '" method="post" id="blog-form">' .
 
-            '<div>' . $this->core->formNonce() . '</div>' .
-            '<p><label class="required" for="blog_id"><abbr title="' . __('Required field') . '">*</abbr> ' . __('Blog ID:') . '</label> ' .
-            Form::field('blog_id', 30, 32,
-                [
-                    'default'    => Html::escapeHTML($blog_id),
-                    'extra_html' => 'required placeholder="' . __('Blog ID') . '"'
-                ]
-            ) . '</p>' .
-            '<p class="form-note">' . __('At least 2 characters using letters, numbers or symbols.') . '</p> ';
+        '<div>' . $this->core->formNonce() . '</div>' .
+        '<p><label class="required" for="blog_id"><abbr title="' . __('Required field') . '">*</abbr> ' . __('Blog ID:') . '</label> ' .
+        Form::field('blog_id', 30, 32,
+            [
+                'default'    => Html::escapeHTML($this->blog_id),
+                'extra_html' => 'required placeholder="' . __('Blog ID') . '"'
+            ]
+        ) . '</p>' .
+        '<p class="form-note">' . __('At least 2 characters using letters, numbers or symbols.') . '</p> ';
 
-            echo
-            '<p><label class="required" for="blog_name"><abbr title="' . __('Required field') . '">*</abbr> ' . __('Blog name:') . '</label> ' .
-            Form::field('blog_name', 30, 255,
-                [
-                    'default'    => Html::escapeHTML($blog_name),
-                    'extra_html' => 'required placeholder="' . __('Blog name') . '" lang="' . $this->core->auth->getInfo('user_lang') . '" ' .
-                        'spellcheck="true"'
-                ]
-            ) . '</p>' .
+        echo
+        '<p><label class="required" for="blog_name"><abbr title="' . __('Required field') . '">*</abbr> ' . __('Blog name:') . '</label> ' .
+        Form::field('blog_name', 30, 255,
+            [
+                'default'    => Html::escapeHTML($this->blog_name),
+                'extra_html' => 'required placeholder="' . __('Blog name') . '" lang="' . $this->core->auth->getInfo('user_lang') . '" ' .
+                    'spellcheck="true"'
+            ]
+        ) . '</p>' .
 
-            '<p><label class="required" for="blog_url"><abbr title="' . __('Required field') . '">*</abbr> ' . __('Blog URL:') . '</label> ' .
-            Form::url('blog_url',
-                [
-                    'size'       => 30,
-                    'default'    => Html::escapeHTML($blog_url),
-                    'extra_html' => 'required placeholder="' . __('Blog URL') . '"'
-                ]
-            ) . '</p>' .
+        '<p><label class="required" for="blog_url"><abbr title="' . __('Required field') . '">*</abbr> ' . __('Blog URL:') . '</label> ' .
+        Form::url('blog_url',
+            [
+                'size'       => 30,
+                'default'    => Html::escapeHTML($this->blog_url),
+                'extra_html' => 'required placeholder="' . __('Blog URL') . '"'
+            ]
+        ) . '</p>' .
 
-            '<p class="area"><label for="blog_desc">' . __('Blog description:') . '</label> ' .
-            Form::textarea('blog_desc', 60, 5,
-                [
-                    'default'    => Html::escapeHTML($blog_desc),
-                    'extra_html' => 'lang="' . $core->auth->getInfo('user_lang') . '" spellcheck="true"'
-                ]) . '</p>' .
+        '<p class="area"><label for="blog_desc">' . __('Blog description:') . '</label> ' .
+        Form::textarea('blog_desc', 60, 5,
+            [
+                'default'    => Html::escapeHTML($this->blog_desc),
+                'extra_html' => 'lang="' . $this->core->auth->getInfo('user_lang') . '" spellcheck="true"'
+            ]) . '</p>' .
 
-            '<p><input type="submit" accesskey="s" name="create" value="' . __('Create') . '" />' .
-            ' <input type="button" value="' . __('Cancel') . '" class="go-back reset hidden-if-no-js" />' .
-            '</p>' .
-            '</form>';
-
-            $this->helpBlock('core_blog_new');
-            $this->close();
-        }
+        '<p><input type="submit" accesskey="s" name="create" value="' . __('Create') . '" />' .
+        ' <input type="button" value="' . __('Cancel') . '" class="go-back reset hidden-if-no-js" />' .
+        '</p>' .
+        '</form>';
     }
 }
