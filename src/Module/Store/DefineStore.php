ea4765651e4c0453ce215f9339221bb571453e0d
Jean-Christian Denis <contact@jcdenis.fr>
2022-01-17 23:30:21 +0100

    clean up

--- src/Module/Store/DefineStore.php
+++ src/Module/Store/DefineStore.php
@@ -12,91 +12,21 @@ declare(strict_types=1);
 
 namespace Dotclear\Module\Store;
 
-use Dotclear\Module\Define;
-use Dotclear\Module\Theme\DefineTheme;
-
-use Dotclear\Html\Html;
+use Dotclear\Module\AbstractDefine;
+use Dotclear\Module\Store\TraitDefineStore;
 
 if (!defined('DOTCLEAR_PROCESS')) {
     return;
 }
 
-class DefineStore extends DefineTheme
+class DefineStore extends AbstractDefine
 {
-    /**
-     * Set module setion
-     *
-     * @param   string  The module section
-     *
-     * @return  Define  Self instance
-     */
-    protected function setSection(string $section): Define
+    use TraitDefineStore;
+
+    protected $type = 'Store';
+
+    protected function checkModule(): void
     {
-        $this->section = $section;
-
-        return $this;
-    }
-
-    /**
-     * Set module tags
-     *
-     * @param   array   The module tags
-     *
-     * @return  Define  Self instance
-     */
-    protected function setTags(array $tags): Define
-    {
-        $this->tags = $tags;
-
-        return $this;
-    }
-
-    /**
-     * Set module score
-     *
-     * @param   int     The module score
-     *
-     * @return  Define  Self instance
-     */
-    protected function setScore(int $score): Define
-    {
-        $this->score = $score;
-
-        return $this;
-    }
-
-    public function section(): string
-    {
-        return $this->properties['section'];
-    }
-
-    public function tags(): array
-    {
-        return $this->properties['tags'];
-    }
-
-    public function score(): int
-    {
-        return $this->properties['score'];
-    }
-
-    protected function checkProperties(): void
-    {
-        if (!isset($this->properties['section'])) {
-            $this->properties['section'] = '';
-        }
-        if (!isset($this->properties['tags'])) {
-            $this->properties['tags'] = [];
-        }
-        foreach($this->properties['tags'] as $k => $v) {
-            if (!is_string($v)) {
-                unset($this->properties['tags'][$k]);
-            }
-        }
-        if (!isset($this->properties['score'])) {
-            $this->properties['score'] = 0;
-        }
-
-        parent::checkProperties();
+        $this->checkDefineStore();
     }
 }
