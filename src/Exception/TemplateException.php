b1e6655003f91a5273ba824205621afc8415e86c
Jean-Christian Denis <contact@jcdenis.fr>
2022-02-06 23:36:13 +0100

    oups

---
+++ src/Exception/TemplateException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @class Dotclear\Exception\TemplateException
+ *
+ * @package Dotclear
+ * @subpackage Exception
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Exception;
+
+use Dotclear\Exception;
+
+class TemplateException extends Exception { }
