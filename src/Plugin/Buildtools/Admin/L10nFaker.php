8fda4db0eb51c6289794f857e53b9fc1eed12525
Jean-Christian Denis <contact@jcdenis.fr>
2022-05-01 15:34:58 +0200

    remove known translations

--- src/Plugin/Buildtools/Admin/L10nFaker.php
+++ src/Plugin/Buildtools/Admin/L10nFaker.php
@@ -12,7 +12,6 @@ namespace Dotclear\Plugin\Buildtools\Admin;
 // Dotclear\Plugin\Buildtools\Admin\L10nFaker
 use Dotclear\App;
 use Dotclear\Helper\File\Path;
-use Dotclear\Plugin\Widgets\Common\WidgetsStack;
 
 /**
  * Buildtools L10n faker.
@@ -39,42 +38,13 @@ class L10nFaker
 
     public function generate_file()
     {
-        $widgets_stack = new WidgetsStack();
-
-        $main   = "<?php\n";
-        $plugin = "<?php\n";
-        $main .= "# Media sizes\n\n";
-        if (!App::core()->media()) {
-            foreach (App::core()->media()->thumb_sizes as $k => $v) {
-                $main .= $this->fake_l10n($v[2]);
-            }
-        }
-        $post_types = App::core()->posttype()->getPostTypes();
-        $main .= "\n# Post types\n\n";
-        foreach ($post_types as $k => $v) {
-            $main .= $this->fake_l10n($v['label']);
-        }
-        $ws = App::core()->user()->preference()->get('favorites'); // Favs old school !
-        if ($ws) {
-            $main .= "\n# Favorites\n\n";
-            foreach ($ws->dumpPrefs() as $k => $v) {
-                $fav = unserialize($v['value']);
-                $main .= $this->fake_l10n($fav['title']);
-            }
-        }
-        file_put_contents(Path::implodeRoot('Core', '_fake_l10n.php'), $main);
-        $plugin .= "\n# Plugin names\n\n";
+        $plugin = "<?php\n\n# Plugin names\n\n";
         foreach ($this->bundled_plugins as $id) {
             $p = App::core()->plugins()?->getModule($id);
             if (!$p) {
                 continue; // cope with dev branch and maybe unknow plugins
             }
-            $plugin .= $this->fake_l10n($p->description());
-        }
-        $plugin .= "\n# Widget settings names\n\n";
-        $widgets = $widgets_stack::$__widgets->elements();
-        foreach ($widgets as $w) {
-            $plugin .= $this->fake_l10n($w->desc());
+            $plugin .= $this->fake_l10n($p->description(false));
         }
         mkdir(Path::implode(__DIR__, '..', '..', '_fake_plugin'));
         file_put_contents(Path::implode(__DIR__, '..', '..', '_fake_plugin', '_fake_l10n.php'), $plugin);
