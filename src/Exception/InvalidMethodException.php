1feaa1c9b433a85e2725ad5956a33c9c18bcdb68
Jean-Christian Denis <contact@jcdenis.fr>
2022-05-13 23:09:59 +0200

    oups add missing Magic Exception handler

---
+++ src/Exception/InvalidMethodException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Exception;
+
+// Dotclear\Exception\InvalidMethodException
+use Exception;
+use Throwable;
+
+/**
+ * Invalid method exception.
+ *
+ * @ingroup  Exception
+ */
+class InvalidMethodException extends Exception
+{
+    public function __construct($message = 'Invalid method call.', $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
