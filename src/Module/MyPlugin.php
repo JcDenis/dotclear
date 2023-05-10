dedaee0196c3673bc32a4801ae8117564a4c48fd
Jean-Christian Denis <contact@jcdenis.fr>
2023-05-11 01:03:39 +0200

    add backend menu icon helper

--- src/Module/MyPlugin.php
+++ src/Module/MyPlugin.php
@@ -16,18 +16,57 @@ declare(strict_types=1);
 
 namespace Dotclear\Module;
 
+use dcAdmin;
 use dcCore;
+use dcMenu;
 use dcModuleDefine;
+use dcPage;
 
 abstract class MyPlugin extends MyModule
 {
     protected static function define(): dcModuleDefine
     {
         if (!(static::$define instanceof dcModuleDefine)) {
-
             static::$define = static::getDefineFromNamespace(dcCore::app()->plugins);
         }
 
         return static::$define;
+    }
+
+    /**
+     * Register backend sidebar menu icon.
+     *
+     * @param   string                  $menu   The menu (from dcAdmin constant)
+     * @param   array<string,string>    $param  The URL params
+     * @param   string                  $scheme the URL end scheme
+     */
+    public static function backendSidebarMenuIcon(string $menu = dcAdmin::MENU_PLUGINS, array $params = [], string $scheme = '(&.*)?$'): void
+    {
+        if (!defined('DC_CONTEXT_ADMIN') || is_null(dcCore::app()->adminurl) || !(dcCore::app()->menu[$menu] instanceof dcMenu)) {
+            return;
+        }
+
+        dcCore::app()->menu[$menu]->addItem(
+            static::name(),
+            dcCore::app()->adminurl->get('admin.plugin.' . static::id(), $params),
+            static::icons(),
+            preg_match('/' . preg_quote(dcCore::app()->adminurl->get('admin.plugin.' . static::id())) . $scheme . '/', $_SERVER['REQUEST_URI']),
+            static::checkContext(static::MENU)
+        );
+    }
+
+    /**
+     * Get modules icon URLs.
+     *
+     * @return  array<string,string>    The module icons URLs
+     */
+    protected static function icons(): array
+    {
+        $icons = [urldecode(dcPage::getPF(static::id() . '/icon.svg'))];
+        if (file_exists(static::path() . DIRECTORY_SEPARATOR . 'icon-dark.svg')) {
+            $icons = [urldecode(dcPage::getPF(static::id() . '/icon-dark.svg'))];
+        }
+
+        retunr $icons;
     }
 }
