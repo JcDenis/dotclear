afb1e2a8439d93816e755770d97b05e74aed6551
Jean-Christian Denis <contact@jcdenis.fr>
2023-05-20 00:07:15 +0200

    use My class for plugins (WIP)

---
+++ plugins/simpleMenu/src/My.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @brief Plugin simpleMenu My module class.
+ * 
+ * A theme My class must extend this class.
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ *
+ * @since 2.27
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Plugin\simpleMenu;
+
+use dcCore;
+use Dotclear\Module\MyPlugin;
+
+class My extends MyPlugin
+{
+}
